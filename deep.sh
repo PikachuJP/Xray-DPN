0ea7864b06f07ff0988d44143a18c58645cf6581
Pikachu Koromi <128409341+PikachuJP@users.noreply.github.com>
2023-08-10 10:07:11 -0700

    Update deep.sh

--- deep.sh
+++ deep.sh
@@ -29,21 +29,21 @@ echo -e "# ${GREEN}YouTube 频道${PLAIN}: https://www.youtube.com/@misaka-blog 
 echo "#############################################################"
 echo ""
 
-yellow "使用前请注意："
-red "1. 我已知悉本项目有可能触发 Deepnote 封号机制"
-red "2. 我不保证脚本其搭建节点的稳定性"
-read -rp "是否安装脚本？ [Y/N]：" yesno
+yellow "Please Note Before Use !"
+red "1. This Might Be Illegal For Github TOS (I Didn't Face Any Issue Till Now) !"
+red "2. It's May Not Be Stable Completely !"
+read -rp "Do You Accept ？ [Y/N]：" yesno
 
 if [[ $yesno =~ "Y"|"y" ]]; then
     rm -f web config.json
-    yellow "开始安装..."
+    yellow "Start Installing..."
     wget -O temp.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
     unzip temp.zip
     rm -f temp.zip
     mv v2ray web
-    read -rp "请设置UUID（如无设置则使用脚本默认的）：" uuid
+    read -rp "Enter UUID (Enter For Defalt Value )" uuid
     if [[ -z $uuid ]]; then
-        uuid="d25b872a-0d06-41b8-8cb3-bade530ae546"
+        uuid="8d4a8f5e-c2f7-4c1b-b8c0-f8f5a9b6c384"
     fi
     rm -f config.json
     cat << EOF > config.json
@@ -94,12 +94,12 @@ if [[ $yesno =~ "Y"|"y" ]]; then
 }
 EOF
     nohup ./web run &>/dev/null &
-    green "Deepnote v2ray 已安装完成！"
-    yellow "请认真阅读项目博客说明文档，配置出站链接！"
-    yellow "别忘记给项目点一个免费的Star！"
+    green "Github Codespaces X2ray Core！"
+    yellow "Please Config Project Before Using It !"
+    yellow "Project Edited By PikachuJP !"
     echo ""
-    yellow "更多项目，请关注：小御坂的破站"
+    yellow "Running !!"
 else
-    red "已取消安装，脚本退出！"
+    red "Canceled !!!!"
     exit 1
 fi
